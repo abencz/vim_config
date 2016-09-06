db2d6e105302e37f0ad07771cae5f36da7d3b5bf
Alex Bencz <abencz@clearpathrobotics.com>
2016-09-06 12:49:51 -0400

    Added support for NeoVim

--- bin/install.sh
+++ bin/install.sh
@@ -1,6 +1,10 @@
 #!/bin/bash
 
 git clone --recursive https://github.com/abencz/vim_config.git ~/.vim
-ln -sf ~/.vim/vimrc ~/.vimrc
+ln -sf ~/.vim/init.vim ~/.vimrc
+
+# support for NeoVim
+mkdir -p ~/.config
+ln -sf ~/.vim ~/.config/nvim
 
 vim +NeoBundleInstall +qall
